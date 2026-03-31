500558d0cc692f51edbf79bf7f933bbe371d6008
Aaron Pham <contact@aarnphm.xyz>
2026-03-31 02:06:40 -0400

    chore: clown shit for bash
    Signed-off-by: Aaron Pham <contact@aarnphm.xyz>

--- hm/modules/atuin.nix
+++ hm/modules/atuin.nix
@@ -13,21 +13,30 @@ with lib; {
     };
   };
 
-  config = mkIf config.atuin.enable {
-    programs.atuin = {
-      enable = true;
-      package = pkgs.atuin;
-      flags = ["--disable-up-arrow"];
-      enableZshIntegration = true;
-      enableBashIntegration = true;
-      settings = {
-        keymap_mode = "vim-insert";
-        auto_sync = true;
-        sync_frequency = "30m";
-        style = "compact";
-        history_filter = ["^gaa" "^gst" "^gcmm"];
-        enter_accept = true;
+  config = mkIf config.atuin.enable (mkMerge [
+    {
+      programs.atuin = {
+        enable = true;
+        package = pkgs.atuin;
+        flags = ["--disable-up-arrow"];
+        enableZshIntegration = true;
+        enableBashIntegration = true;
+        settings = {
+          keymap_mode = "vim-insert";
+          auto_sync = true;
+          sync_frequency = "30m";
+          style = "compact";
+          history_filter = ["^gaa" "^gst" "^gcmm"];
+          enter_accept = true;
+        };
       };
-    };
-  };
+    }
+    (mkIf config.bash.enable {
+      programs.bash.initExtra = mkAfter ''
+        if [[ $- == *i* ]] && declare -F atuin-bind >/dev/null; then
+          atuin-bind -m vi-command '\C-r' atuin-search-vicmd
+        fi
+      '';
+    })
+  ]);
 }
