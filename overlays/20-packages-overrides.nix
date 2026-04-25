4cc87e34b34ba5ef9a452b40e926f093472d41ff
Aaron Pham <contact@aarnphm.xyz>
2026-04-24 22:58:59 -0400

    fix: patch the direnv failure
    Signed-off-by: Aaron Pham <contact@aarnphm.xyz>

--- overlays/20-packages-overrides.nix
+++ overlays/20-packages-overrides.nix
@@ -1,4 +1,25 @@
 {self}: final: prev: {
+  direnv = prev.direnv.overrideAttrs (oldAttrs: {
+    postPatch =
+      (oldAttrs.postPatch or "")
+      + final.lib.optionalString prev.stdenv.hostPlatform.isDarwin ''
+        substituteInPlace test/direnv-test-common.sh \
+          --replace-fail 'direnv_eval() {
+          eval "$(direnv export "$TARGET_SHELL")"
+        }' 'direnv_eval() {
+          local direnvDump
+          direnvDump=$(mktemp)
+          direnv export "$TARGET_SHELL" > "$direnvDump" || true
+          source "$direnvDump"
+          rm -f "$direnvDump"
+        }'
+
+        substituteInPlace GNUmakefile \
+          --replace-fail 'zsh ./test/direnv-test.zsh' \
+            'zsh -f ./test/direnv-test.zsh'
+      '';
+  });
+
   gitstatus = prev.gitstatus.overrideAttrs (oldAttrs: {
     installPhase =
       oldAttrs.installPhase
